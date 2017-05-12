4a38ad8c59a48529cd0189b61945ec9c5d0ec893
Aramys Miranda <aramys@acmeticketing.com>
2017-05-12 15:37:47 -0700

    feat(*): Change replacer to button

--- spectrum.js
+++ spectrum.js
@@ -73,10 +73,10 @@
         return contains(style.backgroundColor, 'rgba') || contains(style.backgroundColor, 'hsla');
     })(),
     replaceInput = [
-        "<div class='sp-replacer'>",
-            "<div class='sp-preview'><div class='sp-preview-inner'></div></div>",
-            "<div class='sp-dd'>&#9660;</div>",
-        "</div>"
+        "<button class='sp-replacer'>",
+            "<span class='sp-preview'><span class='sp-preview-inner'></span></span>",
+            "<span class='sp-dd'>&#9660;</span>",
+        "</button>"
     ].join(''),
     markup = (function () {
 
